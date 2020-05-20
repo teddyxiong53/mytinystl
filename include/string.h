cbad8bc59271aa242072e04e3338ed9030306d91
teddyxiong53 <1073167306@qq.com>
2020-05-20 22:43:15 +0800

    string的可以运行

--- include/string.h
+++ include/string.h
@@ -2,6 +2,7 @@
 #define _TINYSTL_STRING_H_
 
 #include "allocator.h"
+#include "hash.h"
 
 namespace tinystl
 {
@@ -10,7 +11,7 @@ class basic_string {
 public:
     basic_string();
     basic_string(const basic_string& other);
-    basic_string(const basic_string&& other);
+    basic_string(basic_string&& other);
     basic_string(const char *sz);
     basic_string(const char *sz, size_t len);
     ~basic_string();
@@ -41,6 +42,25 @@ private:
 
 };
 
+
+template<typename allocator>
+inline void basic_string<allocator>::clear()
+{
+    resize(0);
+}
+
+template<typename allocator>
+inline void basic_string<allocator>::assign(const char *sz, size_t n)
+{
+    clear();
+    append(sz, sz+n);
+}
+
+template<typename allocator>
+static inline size_t hash(const basic_string<allocator>& value)
+{
+    return hash_string(value.c_str(), value.size());
+}
 template<typename allocator>
 inline basic_string<allocator>::~basic_string()
 {
@@ -122,6 +142,121 @@ inline basic_string<allocator>::basic_string()
    m_capacity(m_buffer + c_nbuffer)
 {
     resize(0);
+}
+template <typename allocator>
+inline size_t basic_string<allocator>::size() const
+{
+    return (size_t)(m_last - m_first);
+}
+
+template<typename allocator>
+inline basic_string<allocator>::basic_string(const basic_string& other)
+ : m_first(m_buffer),
+   m_last(m_buffer),
+   m_capacity(m_buffer+c_nbuffer)
+{
+    reserve(other.size());
+    append(other.m_first, other.m_last);
+}
+
+template<typename allocator>
+inline basic_string<allocator>::basic_string(const char *sz, size_t len)
+ : m_first(m_buffer),
+   m_last(m_buffer),
+   m_capacity(m_buffer + c_nbuffer)
+{
+    reserve(len);
+    append(sz, sz+len);
+}
+template<typename allocator>
+inline basic_string<allocator>::basic_string(basic_string&& other)
+{
+    if(other.m_first == other.m_buffer) {
+        m_first = m_buffer;
+        m_last = m_buffer;
+        m_capacity = m_buffer + c_nbuffer;
+        reserve(other.size());
+        append(other.m_first, other.m_last);
+    } else {
+        m_first = other.m_first;
+        m_last = other.m_last;
+        m_capacity = other.m_capacity;
+    }
+    other.m_first = other.m_last = other.m_buffer;
+    other.m_capacity = other.m_buffer + c_nbuffer;
+    other.resize(0);
+}
+
+template<typename allocator>
+inline basic_string<allocator>& basic_string<allocator>::operator=(const basic_string& other)
+{
+    basic_string(other).swap(*this);
+    return *this;
+}
+
+template<typename allocator>
+inline  basic_string<allocator>& basic_string<allocator>::operator=(basic_string&& other)
+{
+    basic_string(static_cast<basic_string&&>(other)).swap(*this);
+    return *this;
+}
+
+template <typename allocator>
+inline void basic_string<allocator>::swap(basic_string& other)
+{
+    const pointer tfirst = m_first, tlast = m_last, tcapacity = m_capacity;
+    m_first = other.m_first;
+    m_last = other.m_last;
+    m_capacity = other.m_capacity;
+
+    other.m_first = tfirst;
+    other.m_last = tlast;
+    other.m_capacity = tcapacity;
+
+    char tbuffer[c_nbuffer];
+
+    if(m_first == other.m_buffer) {
+        for(pointer it=other.m_buffer, end = m_last, out=tbuffer;
+            it !=end;
+            ++it, ++out)
+        {
+            *out = *it;
+        }
+
+    }
+    if(other.m_first == m_buffer) {
+        other.m_last = other.m_last - other.m_first + other.m_buffer;
+        other.m_first = other.m_buffer;
+        other.m_capacity = other.m_buffer + c_nbuffer;
+
+        for(pointer it=other.m_first, end=other.m_last, in=m_buffer;
+            it != end;
+            ++it, ++in)
+        {
+            *it = *in;
+        }
+        other.m_last = 0;
+    }
+    if(m_first == other.m_buffer) {
+        m_last = m_last - m_first + m_buffer;
+        m_first = m_buffer;
+        m_capacity = m_buffer + c_nbuffer;
+
+        for(pointer it=m_first, end=m_last, in=tbuffer;
+            it != end;
+            ++it, ++in)
+        {
+            *it = *in;
+        }
+        *m_last = 0;
+    }
+
+}
+
+template<typename allocator>
+inline const char * basic_string<allocator>::c_str() const
+{
+    return m_first;
 }
 
 typedef basic_string<TINYSTL_ALLOCATOR> string;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "string.h"
+#include <stdio.h>
+//#include <iostream>
+
 int main(int argc, char const *argv[])
 {
     tinystl::string s1("123");
-    
+    tinystl::string s2(s1);
+    tinystl::string s3 = "123";
+    tinystl::string s4 = s3;
+    printf("s1:%s\n", s1.c_str());
     return 0;
 }
